c8816d0783dab30fe2f3b3a90a74fc45751b9f44
RNubla <loopyelbarto@gmail.com>
2019-12-22 15:02:02 -0500

    Added while loop so that its a continious app. Only stops if user input #3

--- src/main.rs
+++ src/main.rs
@@ -11,25 +11,30 @@ fn main() {
     let username: String = input().msg("Please enter user name:").get();
     println!("{}",username);
 
-    let input_inside: i32 = input().msg("Enter number from 1-3: ").inside(1..=3).get();
+    let mut done = false;
 
-    match input_inside{
-        x if x == 1 => {
-            println!("Addition");
-            let input1: i32 = input().msg("Enter first number to add: ").get();
-            let input2: i32 = input().msg("Enter second number to add: ").get();
-            print!("{} + {} = ", input1, input2);
-            print!("{}",calc(input1, input2));
-        },
-        x if x == 2 => {
-            println!("Hypotenus");
-            let a: f32 = input().msg("a^2: ").get();
-            let b: f32 = input().msg("b^2: ").get();
-            print!("√{} + {} = ", a, b);
-            print!("{}", find_hypotenus(a, b))
-        },
-        3 => println!("Quit"),
-        _ => unreachable!(),
+    while !done {
+        let input_inside: i32 = input().msg("Enter number from 1-3: ").inside(1..=3).get();
+        match input_inside{
+            x if x == 1 => {
+                println!("Addition");
+                let input1: i32 = input().msg("Enter first number to add: ").get();
+                let input2: i32 = input().msg("Enter second number to add: ").get();
+                print!("{} + {} = ", input1, input2);
+                print!("{}\n",calc(input1, input2));
+            },
+            x if x == 2 => {
+                println!("Hypotenus");
+                let a: f32 = input().msg("a^2: ").get();
+                let b: f32 = input().msg("b^2: ").get();
+                print!("c = √{} + {} = ", a, b);
+                print!("{}\n", find_hypotenus(a, b));
+            },
+            x if x == 3 => {
+                done = true;
+            }
+            _ => unreachable!(),
+        }
     }
 }
 
